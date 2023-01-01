f0195e3455d2ca08542efb80efca2e5b2e8d6736
whiteroach <pietro.simcic@gmail.com>
2023-01-01 22:33:04 +0100

    removed blocks with timer from buttons.

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -34,31 +34,34 @@ fn main() -> ! {
     let mut inp_4 = gpioa.pa4.into_pull_down_input(&mut gpioa.crl);
     //Configure pa5 as pull down
     let mut inp_5 = gpioa.pa5.into_pull_down_input(&mut gpioa.crl);
-    //Configure embeded led
+    //Configure embed led
     let mut led = gpioc.pc13.into_push_pull_output(&mut gpioc.crh);
-
+    led.set_low();
 
     // Configure the syst timer to trigger an update every 50ms
     let mut timer = Timer::syst(cp.SYST, &clocks).counter_hz();
     timer.start(1.Hz()).unwrap();//50 ms
-    let mut count = 0;
+
     
 
     loop {
         if inp_4.is_high() {
-            count += 1;
-            defmt::println!("{}",count);
-            block!(timer.wait()).unwrap();
             defmt::println!("4 is high")
         }else if inp_4.is_low(){
-            block!(timer.wait()).unwrap();
             defmt::println!("4 is low low")
         }
         if inp_5.is_high() {
-            block!(timer.wait()).unwrap();
+            // block!(timer.wait()).unwrap();
+            if led.is_set_high() {
+                led.set_low();
+            }
             defmt::println!("5 is high")
         }else {
-            block!(timer.wait()).unwrap();
+            // block!(timer.wait()).unwrap();
+            if led.is_set_low() {
+                led.set_high();
+            }
+
             defmt::println!("5 is low")
         }
     }
